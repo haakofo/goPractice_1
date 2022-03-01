e5168bbdbd1e85e6e21bacb2fdbbb827051cb2c2
Håkon <72071523+haakofo@users.noreply.github.com>
2022-03-01 17:21:23 +0100

    Added handlers and one test handler also structs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,7 +10,41 @@ import (
 
 //Book struct (Model)
 type Book struct {
-	ID string `json: "id`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+//author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+//Function to get all books.
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Fuck you")
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Delete a book.
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -19,10 +54,11 @@ func main() {
 
 	//Route handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books/cock", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe("8000", r))
+	log.Println("Starting server on current_used_port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
