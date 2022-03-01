e7fd46fb6fe368f95bc8b5ddbd681343d28c8250
Håkon <72071523+haakofo@users.noreply.github.com>
2022-03-01 14:35:41 +0100

    Created a main method, router, handlefuncs and a struct

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
-package Practise_1
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Book struct (Model)
+type Book struct {
+	ID string `json: "id`
+}
+
+func main() {
+
+	//Init router
+	r := mux.NewRouter()
+
+	//Route handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe("8000", r))
+}
